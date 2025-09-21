11a5e3c5bb4232fa0bd4558662d8e8106ebd969d
Twixmaximus <134007646+marctina1992@users.noreply.github.com>
2025-09-21 10:39:28 +0000

    chore: de-duplicate after_migrate hook list

--- zanaverse_config/hooks.py
+++ zanaverse_config/hooks.py
@@ -57,7 +57,6 @@ after_migrate = [
     "zanaverse_config.install.apply_email_footer",
     "zanaverse_config.install.apply_onboarding_whitelabel", 
     "zanaverse_config.install.ensure_whitelabel_baseline",
-    "zanaverse_config.install.ensure_whitelabel_baseline"
 ]
 
 update_website_context = "zanaverse_config.brand.update_website_context"
